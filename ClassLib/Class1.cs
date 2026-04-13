690ddb3ed71863c739859374f18f5575d12b6a0b
Student <foobar@sr.htlweiz.at>
2026-04-13 11:29:21 +0200

    andrej240062_chess_commit1

---
+++ ClassLib/Class1.cs
@@ -0,0 +1,35 @@
+﻿namespace ClassLib;
+
+public class Chess
+{
+public override string ToString()
+{
+    var sb = new System.Text.StringBuilder();
+    int size = 8;
+ 
+    string line = "+";
+    for (int i = 0; i < size; i++)
+        line += "--+";
+ 
+    for (int row = 0; row < size; row++)
+    {
+        sb.AppendLine(line);
+ 
+        for (int col = 0; col < size; col++)
+        {
+            sb.Append("|");
+ 
+            if ((row + col) % 2 == 0)
+                sb.Append("  ");
+            else
+                sb.Append("##");
+        }
+ 
+        sb.AppendLine("|");
+    }
+ 
+    sb.AppendLine(line);
+ 
+    return sb.ToString();
+}
+}
---
+++ Program/Program.cs
@@ -0,0 +1,13 @@
+﻿using ClassLib;
+
+namespace ChessGame;
+
+class Program
+{
+    public static void Main()
+    {
+        Chess board = new Chess();
+
+        Console.WriteLine(board.ToString());
+    }
+}
